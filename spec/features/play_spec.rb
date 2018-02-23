7379444d5023e6fea7399372b4cb6c39428bab8c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-02-24 00:12:05 +0100

    Restarting

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,19 +1,9 @@
-=begin
-As a marketeer
-So that I can enjoy myself away from the daily grind
-I would like to be able to play rock/paper/scissors
-=end
-
 feature 'while playing the game' do
   before do
     visit '/'
     fill_in :player_1_name, with: 'Genghis Khan'
     click_button 'Submit'
   end
-
-# As a martekeer
-# So that I can start the game
-# I will be presented with choices (rock, paper and scissors)
 
   scenario 'presented choices of throws Rock, Paper, or Scissors' do
     expect(page).to have_button 'Rock'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 
-# tell Capybara about our app class
 Capybara.app = RockPaperScissors
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
@@ -21,10 +20,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
-RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
-end
+# RSpec.configure do |config|
+#   config.after(:suite) do
+#     puts
+#     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+#     puts "\e[33mTry it now! Just run: rubocop\e[0m"
+#   end
+# end
