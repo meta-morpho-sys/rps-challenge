b9bd73ee9aed598277be7cfa83d1083991a2b00b
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-02-24 15:31:40 +0100

    Backend for starting a game and retrieving Player1 and PLayer2.

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,23 @@
 class Game
   attr_reader :players
 
+  def self.start(player1, player2)
+    @game = Game.new(player1, player2)
+  end
+
+  def self.instance_access
+    @game
+  end
+
   def initialize(player1, player2)
     @players = [player1, player2]
+  end
+
+  def player1
+    @players.first
+  end
+
+  def player2
+    @players.last
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,4 +8,12 @@ describe Game do
   it 'starts with two players' do
     expect(rps.players.size).to eq 2
   end
+
+  example '#player1' do
+    expect(rps.player1).to eq player1
+  end
+
+  example '#player2' do
+    expect(rps.player2).to eq player2
+  end
 end
