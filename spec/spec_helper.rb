7b41c878b4874da453aab75206a4f6875ddf6f9c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-02-24 14:58:53 +0100

    Updated configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,14 @@
-require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
-
 ENV['RACK_ENV'] = 'test'
-
-# require our Sinatra app file
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 Capybara.app = RockPaperScissors
 
