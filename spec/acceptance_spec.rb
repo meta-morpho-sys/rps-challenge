7a73356f5b5290b4a94aa82d7dbf544645f84aaf
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-03-05 11:54:56 +0000

    Feature computer vs human.

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -6,7 +6,9 @@ require 'game'
 describe 'RPS' do
   let(:player1) { Player.new 'Bob' }
   let(:player2) { Player.new 'Ken' }
+  let(:computer) { Player.new 'Computer' }
   let(:game) { Game.start(player1, player2) }
+  let(:computer_vs_human) { Game.start(player1, computer) }
 
   example 'players choose their weapons and win a throw' do
     weapon1 = game.choose_weapon1 'rock'
@@ -18,5 +20,14 @@ describe 'RPS' do
     weapon1 = game.choose_weapon1 'scissors'
     weapon2 = game.choose_weapon2 'rock'
     expect(game.throw(weapon1, weapon2)).to eq(-1)
+  end
+
+  context 'Playing against the computer' do
+    it 'plays automatically against player 1' do
+      weapon1 = computer_vs_human.choose_weapon1 'rock'
+      allow_any_instance_of(Array).to receive(:sample).and_return :scissors
+      weapon2 = computer_vs_human.choose_random_weapon
+      expect(computer_vs_human.throw(weapon1, weapon2)).to eq(1)
+    end
   end
 end
