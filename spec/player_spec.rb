7b24a7375514386a742d801bf3084b001e1ce969
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-03-16 14:06:18 +0000

    Fixed an error in the test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -36,8 +36,8 @@ describe Player do
     end
 
     it '#gain_points' do
-      player1.update(10)
-      expect(player1.points).to eq 10
+      player1.update
+      expect(player1.points).to eq Player::SCORE
     end
   end
 end
